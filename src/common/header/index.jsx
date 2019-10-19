a88dc6a4479c06f83e07c3adcdb620ce2b92ef45
juhengze <18653858844@163.com>
2019-10-19 19:25:24 +0800

    header done

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import JsLoGo from '../../static/jianshu-logo.png';
+import Beta from '../../static/beta.png';
+import './index.less'
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searFocus:false
+        }
+    }
+
+    searchFocus = () => {
+        this.setState({
+            searFocus:true
+        })
+    }
+
+    searchBlur = () => {
+        this.setState({
+            searFocus:false
+        })
+    }
+    
     render(){
-        return <div>
-            
+        let { searFocus } = this.state;
+        return <div className='Header'>
+            <h1><img src={JsLoGo} alt="简书"/></h1>
+            <div className='Hea_rig'>
+                <div className='Hea_zc'>注册</div>
+                <div className='Hea_write'>
+                    <i className="iconfont iconquill"></i>
+                    写文章
+                </div>
+            </div>
+            <div className='Hea_mid'>
+                <span className='Hea_home'><a href='/'>首页</a></span>
+                <span className='Hea_dowApp'><a href='/'>下载App</a></span>
+                <div className={searFocus ? 'Hea_focus_search' : 'Hea_search'}>
+                    <input 
+                        onFocus={this.searchFocus}
+                        onBlur={this.searchBlur}
+                        type="text" 
+                        placeholder="搜索"
+                    />
+                    <i className="iconfont iconfangdajing"></i>
+                </div>
+                <span className='Hea_doload'>登录</span>
+                <img src={Beta} className="Hea_Beta" alt="Beta"/>
+                <i className="iconfont iconAa"></i>
+            </div>
         </div>
     }
 }
